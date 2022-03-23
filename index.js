e84c96a0de0f4202b0630d5f34bb4675222e22bf
Dan Souza <souza.music@outlook.com>
2022-03-23 09:10:50 -0700

    update

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function createEmployeeRecord(){
+    let employee = {
+        firstName: "firstName",
+        familyName: "familyName",
+        title: "title",
+        payPerHour: Number        
+    }
+    // let timeIn = Object.keys([])
+    // let timeOut = Object.keys([])
+    let employeeData = Object.values(employee)
+    console.log(employeeData)
+        return employeeData, timeIn, timeOut
+}
+
+// function createEmployeeRecords(){
+
+// }
