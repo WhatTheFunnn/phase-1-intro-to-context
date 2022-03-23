2f73ed88bf519caba337d9a5be3e22677f40deb2
Dan Souza <souza.music@outlook.com>
2022-03-23 15:49:58 -0700

    update

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-function createEmployeeRecord(employee){
-    let employeeData = {
-        firstName: employee[0],
-        familyName: employee[1],
-        title: employee[2],
-        payPerHour: employee[3],
+function createEmployeeRecord(employeeArray){
+    let employeeObject = {
+        firstName: employeeArray[0],
+        familyName: employeeArray[1],
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
     }
-
-    let timeEvent = {timeInEvents: [], timeOutEvents: []}
-    console.log(employeeData, timeEvent)
-        return [employeeData, timeEvent]
+    return employeeObject
 }
 
-// function createEmployeeRecords(){
+function createEmployeeRecords(employeeArrays){
+    return employeeArrays.map(x => createEmployeeRecord(x))
+}
 
-// }
+function createTimeInEvent(timeInEvents, dateStamp){
+    let timeData = dateStamp.split(" ") 
+    timeInEvents.push(timeData) 
+    console.log(timeInEvents)
+    return timeInEvents
+}
